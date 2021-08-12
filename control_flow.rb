2714ddd87e1fd713baea00685cc4586806c71f59
sam <surickj@gmail.com>
2021-08-11 18:34:11 -0700

    commit tree 6dd53f82694498b44c90507e41a6c67affd981e0

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,64 @@
 def admin_login(username, password)
   # your code here
+  if username=="admin"||"ADMIN"&& password=="12345"
+    "Access granted"
+  else
+    username!="admin"||"ADMIN"&& password!="12345"
+     "Access denied"
+  end
 end
 
-def hows_the_weather(temperature)
-  # your code here
-end
+# def hows_the_weather(temperature)
+#   # your code here
+#   if temperature <40 
+#     puts "It's brisk out there!"
+#   elsif temperature >40 && <65 
+#     puts "It's a little chilly out there!"
+#   elsif temperature > 85
+#     puts "It's too dang hot out there!"
+#   end
+
 
 def fizzbuzz(num)
   # your code here
+  if num ==1 
+    puts num
+  elsif num==2
+    puts num
+  elsif num ==3
+    puts "Fizz"
+  elsif num==4
+    puts num
+  elsif 
+    puts "Buzz"
+  else
+    puts num
+  
+    
+  end
+
 end
 
 def calculator(operation, num1, num2)
   # your code here
+  # if operation == +,-,*,/
+    total = 0
+    switch (operation) 
+      case "+":
+        total = num1+ num2 
+        break;
+      case "-":
+        total = num1 -num2;
+        break;
+      case "*":
+        total = num1* num2";
+        break;
+        default:
+        total = "Reading newspaper.";
+        break;
+    
+
+    
+  end
 end
 
